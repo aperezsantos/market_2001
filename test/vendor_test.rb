5bfd9a18fc67cd481036aada68588feeb4b8b743
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-26 10:22:27 -0700

    Build Vendor and Item test/class files, pass initial tests

---
+++ test/vendor_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+require './lib/item'
+
+class VendorTest < Minitest::Test
+  def test_it_exists
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_instance_of Vendor, vendor
+  end
+
+  def test_it_has_attributes
+    vendor = Vendor.new("Rocky Mountain Fresh")
+
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal ({}), vendor.inventory
+  end
+end
